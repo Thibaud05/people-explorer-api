0817d1fb27d75518323bddd0620d52ef1861c53f
Thibaud GRANIER <thibaud.granier@segeco.fr>
2020-08-04 16:07:17 +0200

    feat(PeopleDataAccess): wikidata sparqlQuery

--- app/Services/PeopleDataAccessService.ts
+++ app/Services/PeopleDataAccessService.ts
@@ -1,11 +1,47 @@
 import PeopleDataAccessInterface from 'Contracts/interfaces/PeopleDataAccess.interface'
+import axios from 'axios'
 
 export default class PeopleDataAccessService implements PeopleDataAccessInterface {
 
-  public getWikiDataPeople(personWikidataId: string[]): object{
+  constructor() {
+    this.endpointUrl = 'https://query.wikidata.org:/sparql'
+  }
+
+  public async getWikiDataPeople(personWikidataIds: string[]): Promise<object> {
+    if (personWikidataIds.length == 0) return []
+
+    const sparqlQuery = `
+    SELECT ?people ?peopleLabel ?typeLabel ?imageLabel ?birthdateLabel  
+    WHERE 
+    {
+      VALUES ?people { 
+        ${this.getPeopleQuery(personWikidataIds)}
+       }
+      ?people wdt:P31 ?type.
+      OPTIONAL{
+        ?people wdt:P569  ?birthdate;
+                wdt:P18   ?image.
+      }
+      
+      SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+    }`
+    let response = await this.queryWikiData(sparqlQuery)
+    return response.data.results.bindings
+  }
+
+  public getPeopleQuery(personWikidataIds: string[]): string {
+    return personWikidataIds.map(personWikidataId => 'wd:' + personWikidataId).join(' ')
+  }
+
+  public getWikiDataSimilarities(personWikidataId: string[]): object {
     return personWikidataId
   }
-  public getWikiDataSimilarities(personWikidataId: string[]): object{
-    return personWikidataId
+
+  public async queryWikiData(sparqlQuery: string): Promise<object> {
+
+    const fullUrl = 'https://query.wikidata.org/sparql' + '?query=' + encodeURIComponent(sparqlQuery)
+    const headers = { 'Accept': 'application/sparql-results+json' }
+
+    return await axios.get(fullUrl, { headers })
   }
 }
--- contracts/interfaces/PeopleDataAccess.interface.ts
+++ contracts/interfaces/PeopleDataAccess.interface.ts
@@ -1,4 +1,6 @@
 export default interface PeopleDataAccessInterface {
     getWikiDataPeople(personWikidataId: string[]): object
     getWikiDataSimilarities(personWikidataId: string[]): object
+    getPeopleQuery(personWikidataIds: string[]): string
+    queryWikiData(sparqlQuery: string): Promise<object>
 }
