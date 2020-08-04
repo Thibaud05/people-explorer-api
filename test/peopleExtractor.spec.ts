6c3e5334137bdd21bbeb7780b07cf59b99414c30
Thibaud GRANIER <thibaud.granier@segeco.fr>
2020-08-04 11:53:02 +0200

    test(peopleExtractor): mockup and invalid key

--- test/peopleExtractor.spec.ts
+++ test/peopleExtractor.spec.ts
@@ -1,16 +1,50 @@
 import test from 'japa'
-
+import sinon from 'sinon'
 
 import PeopleExtractor from '@ioc:App/Services/PeopleExtractor'
+const TextRazor = require('textrazor')
 
-test.group('PeopleExtractor', () => {
-  test('getWikiDataPeople should return Q3052772 and Q22686', (assert) => {
-    let expectedPersonIds = ['Q3052772','Q22686']
-    let personIds = PeopleExtractor.textRazor(`Si, après leurs élections
+test.group('PeopleExtractor textRazor', (group) => {
+
+  group.afterEach(async () => {
+    sinon.restore()
+  }) 
+
+  test('should return the wikidataId of reponse entities', async(assert) => {
+    const text = `Si, après leurs élections
     , on a longtemps parlé de "bromance" pour définir leur relation
     , elle s'est rapidement ternie. A tel point qu'aujourd'hui
-    , Emmanuel Macron et Donald Trump sont devenus adversaires. Et d'ailleurs
-    , il y a quelque chose que Potus ne supporte pas chez le président de la République.`)
+    , Emmanuel Macron et Donald Trump sont devenus adversaires.`
+    const mocukpTextRazorApiResponse = {
+      "response": {
+          "language": "fre",
+          "languageIsReliable": true,
+          "entities": [
+              {
+                  "wikidataId": "Q3052772"
+              },
+              {
+                  "wikidataId": "Q22686"
+              }
+          ]
+      },
+      "time": 0.01568,
+      "ok": true
+  }
+    sinon.stub(TextRazor.prototype, "exec").withArgs(text).returns(mocukpTextRazorApiResponse);
+
+    let expectedPersonIds = ['Q3052772','Q22686']
+    let personIds = await PeopleExtractor.textRazor(text)
     assert.deepEqual(personIds, expectedPersonIds)
   })
+
+  test('should throw exception if invlalid TextRazor API Key', async(assert) => {
+    assert.plan(1)
+
+    try {
+      await PeopleExtractor.textRazor('')
+    } catch (error) {
+      assert.equal(error.error, 'Your TextRazor API Key was invalid.')
+    }
+  }).timeout(6000)
 })
