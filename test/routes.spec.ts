3151860f28db297abdb10365cb5d84b90810064a
Thibaud GRANIER <thibaud.granier@segeco.fr>
2020-08-04 11:54:12 +0200

    test(routes): remove stub after each test

--- test/routes.spec.ts
+++ test/routes.spec.ts
@@ -1,6 +1,6 @@
 import test from 'japa'
 import supertest from 'supertest'
-var sinon = require('sinon')
+import sinon from 'sinon'
 import PeopleDataAccess from '@ioc:App/Services/PeopleDataAccess'
 import PeopleExtractor from '@ioc:App/Services/PeopleExtractor'
 
@@ -25,25 +25,33 @@ const expectedResponse = {
   ]
 }
 
-let stubTextRazor = sinon.stub(PeopleExtractor, "textRazor").withArgs('test').returns(['Q1']);
-let stubGetWikiDataPeople = sinon.stub(PeopleDataAccess, "getWikiDataPeople").withArgs(['Q1']).returns(expectedResponse.people);
-let stubGetWikiDataSimilarities = sinon.stub(PeopleDataAccess, "getWikiDataSimilarities").withArgs(['Q1']).returns(expectedResponse.similarities);
 
-test.group('explore route should', () => {
+
+test.group('explore route should', (group) => {
+
+  group.afterEach(async () => {
+    sinon.restore()
+  }) 
+
+
   test('call PeopleDataAccess razor text with test param and return expectedResponse', async () => {
-
+    sinon.stub(PeopleExtractor, "textRazor").withArgs('test').returns(['Q1']);
+    sinon.stub(PeopleDataAccess, "getWikiDataPeople").withArgs(['Q1']).returns(expectedResponse.people);
+    sinon.stub(PeopleDataAccess, "getWikiDataSimilarities").withArgs(['Q1']).returns(expectedResponse.similarities);
     await supertest(BASE_URL)
       .get('/explore/test')
       .expect(200,JSON.stringify(expectedResponse))
   })
   test('use the cache for the second call with test param', async() => {
-    stubTextRazor.withArgs('test').returns([]);
-    stubGetWikiDataPeople.withArgs('Q1').returns([]);
-    stubGetWikiDataSimilarities.withArgs('Q1').returns([]);
+    sinon.stub(PeopleExtractor, "textRazor").withArgs('test').returns([]);
+    sinon.stub(PeopleDataAccess, "getWikiDataPeople").withArgs('Q1').returns([]);
+    sinon.stub(PeopleDataAccess, "getWikiDataSimilarities").withArgs('Q1').returns([]);
 
     await supertest(BASE_URL)
       .get('/explore/test')
       .expect(200,JSON.stringify(expectedResponse))
   })
+
+
 })
 
