9b84ae14c8799c2be1cba5d043f7a17a0dceaa72
Thibaud GRANIER <thibaud.granier@segeco.fr>
2020-08-04 16:04:59 +0200

    test(PeopleDataAccess): add dataset and empty test

--- test/PeopleDataAccess.spec.ts
+++ test/PeopleDataAccess.spec.ts
@@ -2,15 +2,69 @@ import test from 'japa'
 
 import PeopleDataAccess from '@ioc:App/Services/PeopleDataAccess'
 
-test.group('PeopleDataAccess', () => {
-  test('getWikiDataPeople should return expected people for id Q3 and Q33', (assert) => {
+test.group('PeopleDataAccess getWikiDataPeople', () => {
+  test('should return expected people for id Q3052772 and Q22686', async(assert) => {
     let expectedPeople = [
-
-    ]
-    let people = PeopleDataAccess.getWikiDataPeople(['Q3','Q33'])
+                {
+                    "people": {
+                        "type": "uri",
+                        "value": "http://www.wikidata.org/entity/Q3052772"
+                    },
+                    "peopleLabel": {
+                        "xml:lang": "en",
+                        "type": "literal",
+                        "value": "Emmanuel Macron"
+                    },
+                    "typeLabel": {
+                        "xml:lang": "en",
+                        "type": "literal",
+                        "value": "human",
+                    },
+                    "imageLabel": {
+                        "type": "literal",
+                        "value": "http://commons.wikimedia.org/wiki/Special:FilePath/Emmanuel%20Macron%20%28cropped%29.jpg"
+                    },
+                    "birthdateLabel": {
+                        "type": "literal",
+                        "value": "1977-12-21T00:00:00Z"
+                    }
+                },
+                {
+                    "people": {
+                        "type": "uri",
+                        "value": "http://www.wikidata.org/entity/Q22686"
+                    },
+                    "peopleLabel": {
+                        "xml:lang": "en",
+                        "type": "literal",
+                        "value": "Donald Trump"
+                    },
+                    "typeLabel": {
+                        "xml:lang": "en",
+                        "type": "literal",
+                        "value": "human",
+                    },
+                    "imageLabel": {
+                        "type": "literal",
+                        "value": "http://commons.wikimedia.org/wiki/Special:FilePath/Donald%20Trump%20official%20portrait.jpg"
+                    },
+                    "birthdateLabel": {
+                        "type": "literal",
+                        "value": "1946-06-14T00:00:00Z"
+                    }
+                }
+            ]
+    let people = await PeopleDataAccess.getWikiDataPeople(['Q3052772','Q22686'])
     assert.deepEqual(people, expectedPeople)
   })
-  test('getWikiDataSimilarities should return expected expected similarities for id Q3 and Q33', (assert) => {
+  test('should return empty array for empty array', async(assert) => {
+    let expectedPeople = []
+    let people = await PeopleDataAccess.getWikiDataPeople([])
+    assert.deepEqual(people, expectedPeople)
+  })
+})
+test.group('PeopleDataAccess getWikiDataSimilarities', () => {
+  test('should return expected expected similarities for id Q3 and Q33', (assert) => {
     let expectedSimilarities = [
       {
         personWikidataId: "Q3052772",
