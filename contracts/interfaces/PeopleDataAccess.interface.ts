dda805357ddf018e9f851a398f8a89e12b10ac28
Thibaud GRANIER <thibaud.granier@segeco.fr>
2020-08-04 16:22:26 +0200

    feat(PeopledataAccess): split fetch and format

--- contracts/interfaces/PeopleDataAccess.interface.ts
+++ contracts/interfaces/PeopleDataAccess.interface.ts
@@ -1,5 +1,7 @@
 export default interface PeopleDataAccessInterface {
     getWikiDataPeople(personWikidataId: string[]): object
+    fetchWikiDataPeople((personWikidataId: string[]): object
+    formatWikiDataPeople(object): object
     getWikiDataSimilarities(personWikidataId: string[]): object
     getPeopleQuery(personWikidataIds: string[]): string
     queryWikiData(sparqlQuery: string): Promise<object>
