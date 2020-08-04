530240708e0deffcf07773a83bfac464ef977647
Thibaud GRANIER <thibaud.granier@segeco.fr>
2020-08-04 17:00:19 +0200

    feat(PeopleDataAccess): formatWikiDataPeople

--- app/Services/PeopleDataAccessService.ts
+++ app/Services/PeopleDataAccessService.ts
@@ -3,11 +3,19 @@ import axios from 'axios'
 
 export default class PeopleDataAccessService implements PeopleDataAccessInterface {
 
+  endpointUrl : string
+
   constructor() {
     this.endpointUrl = 'https://query.wikidata.org:/sparql'
   }
 
   public async getWikiDataPeople(personWikidataIds: string[]): Promise<object> {
+    let wikiDataPeople = await this.fetchWikiDataPeople(personWikidataIds)
+    wikiDataPeople = this.formatWikiDataPeople(wikiDataPeople)
+    return wikiDataPeople
+  }
+
+  public async fetchWikiDataPeople(personWikidataIds: string[]): Promise<object>{
     if (personWikidataIds.length == 0) return []
 
     const sparqlQuery = `
@@ -28,6 +36,22 @@ export default class PeopleDataAccessService implements PeopleDataAccessInterfac
     let response = await this.queryWikiData(sparqlQuery)
     return response.data.results.bindings
   }
+
+  public formatWikiDataPeople(wikiDataPeople): object{
+
+    return wikiDataPeople.map(wikiDataPerson => {
+        return {
+          wikidataId:wikiDataPerson?.people?.value,
+          label:wikiDataPerson?.peopleLabel?.value,
+          type:wikiDataPerson?.typeLabel?.value,
+          image:wikiDataPerson?.imageLabel?.value,
+          birthdate:wikiDataPerson?.birthdateLabel?.value
+        }
+      }
+    )
+  }
+
+
 
   public getPeopleQuery(personWikidataIds: string[]): string {
     return personWikidataIds.map(personWikidataId => 'wd:' + personWikidataId).join(' ')
