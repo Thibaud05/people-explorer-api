c651d408a0c6c50100ee5d4e5d7bed3172a20b09
Thibaud GRANIER <thibaud.granier@segeco.fr>
2020-08-05 17:24:45 +0200

    test: add tests for code response

--- test/routes.spec.ts
+++ test/routes.spec.ts
@@ -25,9 +25,7 @@ const expectedResponse = {
   ]
 }
 
-
-
-test.group('explore route should', (group) => {
+test.group('/ route should', (group) => {
 
   group.afterEach(async () => {
     sinon.restore()
@@ -37,20 +35,43 @@ test.group('explore route should', (group) => {
     sinon.stub(PeopleExtractor, "textRazor").withArgs('test').returns(Promise.resolve(['Q1']));
     sinon.stub(PeopleDataAccess, "getWikiDataPeople").withArgs(['Q1']).returns(Promise.resolve(expectedResponse.people))
     sinon.stub(PeopleDataAccess, "getWikiDataSimilarities").withArgs(['Q1']).returns(Promise.resolve(expectedResponse.similarities))
+
     await supertest(BASE_URL)
-      .get('/explore/test')
+      .get('/test')
       .expect(200,expectedResponse)
   })
+
+  test('return 404 if no person found', async() => {
+    sinon.stub(PeopleExtractor, "textRazor").withArgs('test404').returns(Promise.resolve([]));
+
+    await supertest(BASE_URL)
+      .get('/test404')
+      .expect(404,{code:404,error:'no person found'})
+  })
+
   test('use the cache for the second call with test param', async() => {
     sinon.stub(PeopleExtractor, "textRazor").withArgs('test').returns(Promise.resolve([]));
     sinon.stub(PeopleDataAccess, "getWikiDataPeople").withArgs(['Q1']).returns(Promise.resolve([]))
     sinon.stub(PeopleDataAccess, "getWikiDataSimilarities").withArgs(['Q1']).returns(Promise.resolve([]))
 
     await supertest(BASE_URL)
-      .get('/explore/test')
+      .get('/test')
       .expect(200,expectedResponse)
   })
 
+  test('return 400 : Bad Request for an emtpy text ', async() => {
+
+    await supertest(BASE_URL)
+      .get('/%20')
+      .expect(400,{code:400,error:'Bad Request'})
+  })
+
+  test('return 401: Unauthorized for a bad textrazor key ', async() => {
+    sinon.stub(process.env, 'TEXTRAZOR_API_KEY').value('badKey')
+    
+    await supertest(BASE_URL)
+      .get('/badApiKey')
+      .expect(401,{code:401,error:'Your TextRazor API Key was invalid.'})
+  }).timeout(6000)
 
 })
-
